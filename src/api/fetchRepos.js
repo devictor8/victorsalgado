30d5dbc62f0f4ffd5790cb7cabf94e2eb3a2d04d
devictor8 <joaosalgado605@gmail.com>
2024-02-16 15:55:36 -0300

    reset project

--- src/api/fetchRepos.js
+++
@@ -1,8 +0,0 @@
-const fetchRepos = async () => {
-	const response = await fetch('https://api.github.com/users/devictor8/repos');
-	const data = await response.json();
-
-	return data;
-};
-
-export default fetchRepos;
