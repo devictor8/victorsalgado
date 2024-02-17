110f561a8eb0d452132bf07d69b891f1be5887d6
devictor8 <joaosalgado605@gmail.com>
2024-02-17 16:25:01 -0300

    add: Foi criada a interface de modo escuro e foi adicionando alguns estilos do modo escuro aos componentes

--- src/components/DarkModeMenu.jsx
+++ src/components/DarkModeMenu.jsx
@@ -1,13 +1,84 @@
-import React from 'react';
-import { CgDarkMode } from 'react-icons/cg';
+import React, { useEffect, useState } from 'react';
+import { FaRegMoon, FaRegSun, FaDesktop } from 'react-icons/fa';
 
 export default function DarkModeMenu() {
-  const handleTheme = () => {
+  // document.documentElement.classList.add('dark');
+  const [isOpen, setIsOpen] = useState(false);
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ? localStorage.theme : 'system',
+  );
+  const element = document.documentElement;
+  const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  const options = [
+    {
+      icon: <FaRegMoon className="mr-2" />,
+      text: 'dark',
+    },
+    {
+      icon: <FaRegSun className="mr-2" />,
+      text: 'light',
+    },
+    {
+      icon: <FaDesktop className="mr-2" />,
+      text: 'system',
+    },
+  ];
+  const onWindowMatch = () => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && darkQuery.matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   };
-
+  onWindowMatch();
+  useEffect(() => {
+    switch (theme) {
+      case 'dark':
+        element.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      case 'light':
+        element.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        break;
+      default:
+        localStorage.removeItem('theme');
+        onWindowMatch();
+        break;
+    }
+  }, [theme]);
+  const handleMenuOpen = () => setIsOpen(!isOpen);
+  darkQuery.addEventListener('change', (event) => {
+    if (!(theme in localStorage)) {
+      if (event.matches) {
+        element.classList.add('dark');
+      } else {
+        element.classList.remove('dark');
+      }
+    }
+  });
   return (
-    <div>
-      <button type="button"><CgDarkMode className="text-4xl" /></button>
+    <div className="m-3">
+      <button type="button" onClick={handleMenuOpen} className="text-3xl text-color-default">
+        <FaRegSun />
+      </button>
+
+      <div
+        className={`dark:bg-zinc-900 bg-slate-100 rounded-md py-1 fixed 
+        ${isOpen ? '' : 'hidden'}`}
+      >
+        {options.map((item) => (
+          <div
+            key={item.text}
+            onClick={() => setTheme(item.text)}
+            className={`themeIterface 
+          ${theme === item.text ? 'bg-slate-200 text-slate-900 font-bold dark:bg-zinc-800' : ''}`}
+          >
+            {item.icon}
+            {item.text[0].toUpperCase() + item.text.slice(1)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
